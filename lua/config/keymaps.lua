d6c488d559c73c6ac9ec83b7d1292b1e5a371823
Srikar Vanavasam <svanavasam3@gatech.edu>
2023-07-18 05:00:28 -0500

    Initial updates
    updates
    
    Update keymaps.lua
    
    tab cmp
    
    toggle highlight
    
    copilot?
    
    fix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.g.maplocalleader = ","
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "<C-p>", '"0p')
+vim.keymap.set("n", "<C-P>", '"0P')
+vim.cmd("map 0 ^")
+vim.keymap.set("t", "<esc>", [[<C-\><C-n>]]) -- Exit from insert mode by Esc in Terminal
+vim.keymap.set("n", "<Leader>h", ":noh<cr>")
